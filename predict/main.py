f5541e02eafe1b4623396e7da8464f1257b1c294
Belavin Vladislav <belavin@phystech.edu>
2016-11-21 19:03:58 +0300

    Заглушка в main.py
    В случае не совпадения данных на входе и ожидаемых(тип или ещё что-то), всё не падает, а выдаёт некоторое случайное число.

--- predict/main.py
+++ predict/main.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import request
 import json
+import random
 #import boosting
 app = Flask(__name__)
 
@@ -19,16 +20,18 @@ def process_json():
 			f.write( data + "\n" )
 	except:
 		print "Bad json", data
-		return "Bad json, " + str(data)
+		return random.random()
+		#return "Bad json, " + str(data)
 
-	# try:
-	score = 0.4 + float(j["exam_points"]) * 0.2 / 300.0  - float(j["friends"]) * 0.05 / 1500.0 + (j["department"] == u"ФОПФ") * 0.13 - (j["department"] == u"ФИВТ") * 0.11  + (j["exam retakes"] == u"Не больше трёх") * 0.07 - (j["lectures"] == u"Очень редко") * 0.03
-	score = min(score, 0.95)
-	score = max(score, 0.05)
+	try:
+		score = 0.4 + float(j["exam_points"]) * 0.2 / 300.0  - float(j["friends"]) * 0.05 / 1500.0 + (j["department"] == u"ФОПФ") * 0.13 - (j["department"] == u"ФИВТ") * 0.11  + (j["exam retakes"] == u"Не больше трёх") * 0.07 - (j["lectures"] == u"Очень редко") * 0.03
+		score = min(score, 0.95)
+		score = max(score, 0.05)
 
-	# except:
-	# 	print "Bad data", data
-	# 	return "Bad data, " + str(data)
+	except:
+	 	print "Bad data", data
+		return random.random() 
+	 	#return "Bad data, " + str(data)
 
 
 	return str(score)
