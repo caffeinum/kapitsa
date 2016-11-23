8a88ed31733d63bd9672eaafa558e2aac94b05b3
Belavin Vladislav <belavin@phystech.edu>
2016-11-23 20:56:54 +0300

    Adding a handler for the 'uncaughtException'
    From the site: "Note that 'uncaughtException' is a crude mechanism for exception handling intended to be used only as a last resort."
    
    Ahh. We don't have a choice.

--- server.js
+++ server.js
@@ -51,3 +51,14 @@ app.listen(80, function () {
 	console.log('app listening on port 80!')
 })
 
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
+
+setTimeout(function () {
+  console.log('This will still run.');
+}, 500);
+
+// Intentionally cause an exception, but don't catch it.
+nonexistentFunc();
+console.log('This will not run.');
